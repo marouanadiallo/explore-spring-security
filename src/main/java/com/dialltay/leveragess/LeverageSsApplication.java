4c48f079840981699bfeef59dfee6e1aaa293ae2
marouana.diallo <marouanadiallo1@gmail.com>
2025-05-01 10:37:40 +0200

    init project

---
+++ src/main/java/com/dialltay/leveragess/LeverageSsApplication.java
@@ -0,0 +1,13 @@
+package com.dialltay.leveragess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeverageSsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeverageSsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dialltay/leveragess/LeverageSsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dialltay.leveragess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeverageSsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
